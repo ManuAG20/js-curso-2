99b46a3d258662c034db202c3441bcff356350a0
Manu <manukorox@gmail.com>
2024-01-23 12:14:18 -0500

    desafio

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+let titulo=document.querySelector('h1');
+titulo.innerHTML = 'Hora del Desafío';
 
+function intentoAlert (){
+    alert('Yo amo JS');
+}
+function intentoConsole (){
+    console.log('El botón fue clicado');
+}
+function intentoPrompt (){
+    let ciudad=prompt('escriba la ciudad de brasil que mas te gusta');
+    alert(`Estuve en ${ciudad} y me acorde de ti`);
+}
+function intentoSuma (){
+    let numero1=parseInt(prompt('Ingresa el primer numero'));
+    let numero2=parseInt(prompt('Ingresa el segundo numero'));
+    let suma= numero1 + numero2;
+    alert('El resultado es : '+ suma);
+}
